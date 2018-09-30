418c6eb929b0f428fad86fe63129f783ea2fdf49
Jonathan_Ishii <jtishii@yahoo.com>
2018-09-29 18:06:46 -0700

    Initial Commit

---
+++ Rat18F-Compiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rat18F-Compiler
+//
+//  Created by Jonathan Ishii on 9/29/18.
+//  Copyright © 2018 Jonathan Ishii. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
